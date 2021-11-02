f4eecf5f80d8a4987f477d0564609f1802f1cff5
Hyeju <Hyeju5891@gmail.com>
2021-11-01 20:46:10 -0700

    npm

---
+++ app.js
@@ -0,0 +1,19 @@
+// user/cpnt.log
+{
+console.log("CPNT-a4");
+"CPTNT-a4"
+}
+    "name": "CPNT-a4",
+    "vision": "1.0.0",
+    "description": "Node comand line app",
+    "main": "app.js",
+    "scripts": {
+    "test": "echo /"Error: no test specifited / " &&exit 1 "
+},
+"keywords:[]",
+ "author": Hyeju Hong "(https://github.com/Hyeju1996)",
+    "licence": "MIT",
+    "CPNT-a4" {
+}    
+ 
+const {}
